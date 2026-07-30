f29381906bdd5daba6f8ee0799561eadab259403
Abhishek <abhishekpatil4907@gmail.com>
2026-07-30 13:47:59 +0530

    Add files via upload

---
+++ star pattern1.py
@@ -0,0 +1,15 @@
+for i in range(1,6):
+    for j in range(i):
+        print("1",end=" ")
+    
+            
+    print()
+    
+print()    
+for i in range(6,0,-1):
+    for j in range(i):
+        print("1",end=" ")
+    print()
+    
+    
+ 
