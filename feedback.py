d12f5f6c5b97af642cc0cc2550684ffec0a0e3d1
Abhishek <abhishekpatil4907@gmail.com>
2026-08-13 14:18:26 +0530

    Add files via upload

---
+++ feedback.py
@@ -0,0 +1,17 @@
+print("customer feedback report".upper().center(50))
+print("================================".center(50))
+feedback = "hellow firend my name is abhishek"
+print("original feedback".center(50).title())
+print("feedback".center(50))
+
+print("******************************".cemter(50))
+print("feedback summary".upper().center(50))
+
+
+
+print(len(feedback))
+print(len(feedback.split()))
+print(feedback.count(" "))
+print()
+print(feedback.lower())
+print(feedback.upper())
