824c3d177a75f0c51734eadcff54204c484bf7c2
Abhishek <abhishekpatil4907@gmail.com>
2026-08-13 14:18:09 +0530

    Add files via upload

---
+++ Feedback formator.py
@@ -0,0 +1,16 @@
+Customer_name=input("Enter Customer name:")
+product_name=input("Enter product name:")
+feedback=input("Enter your feedback:")
+
+
+
+custer_name=Customer_name.strip().title()
+product_name=product_name.strip().title()
+Feeback=feedback.capitalize().strip()
+
+print("##########FEEDBACK##########")
+print("customer name:",Customer_name)
+print("product name:",product_name)
+print("feedback:",feedback)
+
+print("========Thankyou for your Feedback===========")
